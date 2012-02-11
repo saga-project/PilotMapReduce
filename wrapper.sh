05277bfd527c95d38f77e538992110add55bb4ee
pradeepmantha <pradeepm66@gmail.com>
2012-02-11 15:40:07 -0500

    added pilot mapreduce script

---
+++ wrapper.sh
@@ -0,0 +1,7 @@
+rm -fr /N/u/pmantha/temp/*
+rm -fr /N/u/pmantha/agent/*
+rm -fr /N/u/pmantha/output/*
+ssh sierra.futuregrid.org rm -fr /N/u/pmantha/temp/*
+ssh sierra.futuregrid.org rm -fr /N/u/pmantha/agent/*
+ssh sierra.futuregrid.org rm -fr /N/u/pmantha/output/*
+python  DMRwithPD.py
