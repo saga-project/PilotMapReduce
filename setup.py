95306ce1783131e5d03052ed6bd30f9c827b8826
Pradeep Kumar Pradeep Kumar <pmantha@login2.uc.futuregrid.org>
2012-11-05 02:11:59 -0600

    fix

--- setup.py
+++ setup.py
@@ -82,13 +82,12 @@ setup_args = {
         ],
 
     'packages': [
-        "pmr"
+        "pmr","applications"
     ],
     'scripts': scripts,
     # mention data_files, even if empty, so install_data is called and
     # VERSION gets copied
-    'data_files': [("pmr", []),
-                   ("applications",[])],
+    'data_files': [("", ["README","README"])],
     'cmdclass': {
         'install_data': our_install_data,
         'sdist': our_sdist
